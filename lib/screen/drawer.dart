bf35e5a9f2121a8121a1df802739225a4e33a29a
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-03-01 19:09:58 +0900

    reformat

--- lib/screen/drawer.dart
+++ lib/screen/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:asterfox/screen/screens/debug_screen.dart';
 import 'package:asterfox/theme/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -10,55 +11,84 @@ class SideMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
         width: 210,
-        child: Row(children: [
-          Expanded(
+        child: Row(
+          children: [
+            Expanded(
               child: Drawer(
-                  child: Container(
-                    color: Theme.of(context).scaffoldBackgroundColor,
-            child: SingleChildScrollView(
-              child: ListView(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                children: [
-                  DrawerHeader(
-                      child: Center(
-                    child: ListTile(
-                      leading: Image.asset(
-                        "assets/images/asterfox.png",
-                        scale: 0.1,
-                      ),
-                      title: const Text(
-                        "Asterfox",
-                        textScaleFactor: 1.3
-                      ),
-                      ),
-                    ),
-                  ),
-                  DrawerListTile(title: "Home", icon: Icons.home, press: () {}),
-                  DrawerListTile(
-                      title: "Playlist",
-                      icon: Icons.playlist_play,
-                      press: () {}),
-                  DrawerListTile(
-                      title: "Playback", icon: Icons.replay, press: () {}),
-                  DrawerListTile(
-                      title: "Settings", icon: Icons.settings, press: () {}),
-                ],
-              ),
-            ),
-          ))),
-        ]));
+                child: Container(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  child: SingleChildScrollView(
+                    child: ListView(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        DrawerHeader(
+                          child: Center(
+                            child: ListTile(
+                              leading: Image.asset(
+                                "assets/images/asterfox.png",
+                                scale: 0.1,
+                              ),
+                              title: const Text(
+                                "Asterfox",
+                                textScaleFactor: 1.3
+                              ),
+                            ),
+                          ),
+                        ),
+                        DrawerListTile(
+                          title: "Home",
+                          icon: Icons.home,
+                          onPressed: () {}
+                        ),
+                        DrawerListTile(
+                          title: "Playlist",
+                          icon: Icons.playlist_play,
+                          onPressed: () {}
+                        ),
+                        DrawerListTile(
+                          title: "Playback",
+                          icon: Icons.replay,
+                          onPressed: () {}
+                        ),
+                        DrawerListTile(
+                          title: "Settings",
+                          icon: Icons.settings,
+                          onPressed: () {}
+                        ),
+                        DrawerListTile(
+                          title: "Debug",
+                          icon: Icons.bug_report,
+                          onPressed: () {
+                            // close drawer
+                            Navigator.pop(context);
+
+                            //open the debug page
+                            Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => const DebugScreen())
+                            );
+                          }
+                        )
+                      ]
+                    )
+                  )
+                )
+              )
+            )
+          ]
+        )
+    );
   }
 }
 
 class DrawerListTile extends StatelessWidget {
   const DrawerListTile(
-      {required this.title, required this.icon, required this.press, Key? key})
+      {required this.title, required this.icon, required this.onPressed, Key? key})
       : super(key: key);
 
   final String title;
   final IconData icon;
-  final VoidCallback press;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +96,7 @@ class DrawerListTile extends StatelessWidget {
       color: Theme.of(context).scaffoldBackgroundColor,
       child: InkWell(
         child: ListTile(
-          onTap: press,
+          onTap: onPressed,
           horizontalTitleGap: 0.0,
           leading: Icon(icon, color: Theme.of(context).textTheme.headline3!.color),
           title: Text(
