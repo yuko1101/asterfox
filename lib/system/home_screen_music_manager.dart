eda174775811e975f5ac59d3e29b3b85df8a9481
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-05-02 14:12:40 +0900

    changed loading indicator

--- lib/system/home_screen_music_manager.dart
+++ lib/system/home_screen_music_manager.dart
@@ -1,27 +1,25 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../main.dart';
 import '../music/audio_source/youtube_audio.dart';
 import '../screen/screens/home_screen.dart';
 import '../util/in_app_notification/notification_data.dart';
 import '../util/youtube_music_utils.dart';
-import '../widget/theme_widget.dart';
 
 class HomeScreenMusicManager {
   static Future<void> addSongByID(String id) async {
     final completer = Completer();
     homeNotification.pushNotification(
         NotificationData(
-            title: Row(
-              children: [
-                ThemeWidget(
-                  builder: (_, theme) => SpinKitCubeGrid(
-                    size: 10,
-                    color: theme.textTheme.bodyText1?.color,
-                  ),
+            child: Row(
+              children: const [
+                SizedBox(
+                  width: 24,
+                  height: 24,
+                  child: CircularProgressIndicator()
                 ),
-                const Text("1曲を読み込み中"),
+                SizedBox(width: 8),
+                Text("1曲を読み込み中"),
               ],
             ),
             progress: () async {
