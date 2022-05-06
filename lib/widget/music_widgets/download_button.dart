6018a3c690964b0e3b42a4aeaef421bc5925839f
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-05-06 20:40:09 +0900

    made better and made refresh button available

--- lib/widget/music_widgets/download_button.dart
+++ lib/widget/music_widgets/download_button.dart
@@ -39,40 +39,53 @@ class DownloadButton extends StatelessWidget {
             }
             if (isDownloaded) {
               return IconButton(
-                icon: const Icon(Icons.file_download_done),
+                icon: const Icon(Icons.refresh),
                 onPressed: () {
-                  // TODO: refresh to local
+                  musicManager.refreshSongs(musicManager.currentIndexNotifier.value!);
                 },
               );
             }
-            return IconButton(
-                onPressed: downloadable ? () {
-                  downloadProgress[song!.key!] = ValueNotifier<int>(0);
-                  homeNotification.pushNotification(
-                    NotificationData(
-                      child: ValueListenableBuilder<int>(
-                        valueListenable: downloadProgress[song.key!]!,
-                        builder: (_, percentage, __) => Column(
-                          children: [
-                            const Text("ダウンロード中"),
-                            SizedBox(
-                              width: 100,
-                              child: LinearProgressIndicator(
-                                minHeight: 8,
-                                value: percentage / 100,
-                                color: CustomColors.getColor("accent"),
-                                backgroundColor: CustomColors.getColor("accent").withOpacity(0.1),
-                              ),
+            if (downloadable) {
+              return IconButton(
+                  onPressed: () {
+                    downloadProgress[song!.key!] = ValueNotifier<int>(0);
+                    homeNotification.pushNotification(
+                      NotificationData(
+                          child: ValueListenableBuilder<int>(
+                            valueListenable: downloadProgress[song.key!]!,
+                            builder: (_, percentage, __) => Column(
+                              children: [
+                                const Text("ダウンロード中"),
+                                SizedBox(
+                                  width: 100,
+                                  child: LinearProgressIndicator(
+                                    minHeight: 8,
+                                    value: percentage / 100,
+                                    color: CustomColors.getColor("accent"),
+                                    backgroundColor: CustomColors.getColor("accent").withOpacity(0.1),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                          progress: () async => await MusicDownloader.download(song)
                       ),
-                      progress: () async => await MusicDownloader.download(song)
-                    ),
-                  );
-                } : null,
-                icon: const Icon(Icons.file_download)
+                    );
+                  },
+                  icon: const Icon(Icons.file_download)
+              );
+            }
+            if (song != null && song.isLocal) {
+              return const IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.file_download_done)
+              );
+            }
+            return const IconButton(
+                onPressed: null,
+                icon: Icon(Icons.file_download)
             );
+
           }
         );
       }
