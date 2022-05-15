5882fd40a255b506cc321b506271d877ff4ae536
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-05-15 19:51:56 +0900

    fixed #7 and show notification on adding songs with sharing intent

--- lib/system/sharing_intent.dart
+++ lib/system/sharing_intent.dart
@@ -1,4 +1,5 @@
 import 'package:asterfox/main.dart';
+import 'package:asterfox/system/home_screen_music_manager.dart';
 import 'package:asterfox/util/youtube_music_utils.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
@@ -8,17 +9,20 @@ import '../music/audio_source/youtube_audio.dart';
 
 class SharingIntent {
   static void init() {
-    ReceiveSharingIntent.getTextStream().listen((String text) async {
-      VideoId id;
-      try {
-        id = VideoId(text);
-      } catch (e) {
-        Fluttertoast.showToast(msg: "無効なURLです");
-        return;
-      }
-      final YouTubeAudio? song = await YouTubeMusicUtils.getYouTubeAudio(id.value);
-      if (song == null) return;
-      await musicManager.add(song);
-    });
+    ReceiveSharingIntent.getTextStream().listen(addSong);
+
+    ReceiveSharingIntent.getInitialText().then(addSong);
+  }
+
+  static Future<void> addSong(String? text) async {
+    if (text == null) return;
+    VideoId id;
+    try {
+      id = VideoId(text);
+    } catch (e) {
+      Fluttertoast.showToast(msg: "無効なURLです");
+      return;
+    }
+    HomeScreenMusicManager.addSongByID(id.value);
   }
 }
