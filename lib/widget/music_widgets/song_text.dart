011904ccaad7311fd90acb56f6ade037d7c8593e
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-06-11 18:04:30 +0900

    update scroll physics

--- lib/widget/music_widgets/song_text.dart
+++ lib/widget/music_widgets/song_text.dart
@@ -12,6 +12,7 @@ class CurrentSongTitle extends StatelessWidget {
         return Padding(
           padding: const EdgeInsets.only(top: 8.0),
           child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
               scrollDirection: Axis.horizontal,
               child: Text(musicData?.title ?? "", style: const TextStyle(fontSize: 20))
           ),
@@ -32,6 +33,7 @@ class CurrentSongAuthor extends StatelessWidget {
           padding: const EdgeInsets.only(top: 8.0),
           child:
           SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
             scrollDirection: Axis.horizontal,
             child: Text(musicData?.author ?? "", style: const TextStyle(fontSize: 18, color: Colors.grey))
           ),
