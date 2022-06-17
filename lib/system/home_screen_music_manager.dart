cd929e4f1d948d999779cc42c0a5ddb2cd164620
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-06-17 20:09:41 +0900

    moved methods

--- lib/system/home_screen_music_manager.dart
+++ lib/system/home_screen_music_manager.dart
@@ -4,13 +4,14 @@ import 'package:asterfox/data/local_musics_data.dart';
 import 'package:asterfox/data/settings_data.dart';
 import 'package:asterfox/main.dart';
 import 'package:asterfox/music/audio_source/music_data.dart';
-import 'package:asterfox/music/audio_source/youtube_music_data.dart';
 import 'package:asterfox/music/music_downloader.dart';
+import 'package:asterfox/utils/extensions.dart';
 import 'package:asterfox/utils/youtube_music_utils.dart';
 import 'package:easy_app/utils/languages.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:easy_app/utils/in_app_notification/notification_data.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 import '../screens/home_screen.dart';
 
@@ -162,8 +163,52 @@ class HomeScreenMusicManager {
   }
 
   static Future<void> addSongBySearch(String query) async {
+    if (query.isUrl) {
+      await loadFromUrl(query);
+      return;
+    }
     final list = await YouTubeMusicUtils.searchYouTubeVideo(query);
     await addSong(key: const Uuid().v4(), youtubeId: list.first.id.value);
+  }
+
+  static Future<void> loadFromUrl(String url) async {
+    final isPlaylist = await loadPlaylist(url);
+    if (isPlaylist) return;
+
+    VideoId id;
+    try {
+      id = VideoId(url);
+    } on ArgumentError {
+      HomeScreen.homeNotification.pushNotification(
+        NotificationData(
+          child: Text(Language.getText("invalid_url")),
+        )
+      );
+      return;
+    }
+    await addSong(key: const Uuid().v4(), youtubeId: id.value);
+  }
+
+
+  static final RegExp playlistRegex = RegExp(r"^https?://(www.)?youtube.com/playlist\?((.+=.+&)*)list=([^&]+)");
+  static Future<bool> loadPlaylist(String text) async {
+    if (!playlistRegex.hasMatch(text)) {
+      return false;
+    }
+    final match = playlistRegex.firstMatch(text)!;
+    final listId = match.group(4)!;
+    final yt = YoutubeExplode();
+    final playlist = await yt.playlists.get(listId);
+    if (playlist.videoCount == null || playlist.videoCount == 0) {
+      HomeScreen.homeNotification.pushNotification(
+          NotificationData(
+            child: Text(Language.getText("external_playlist_empty")),
+          )
+      );
+      return true;
+    }
+    await HomeScreenMusicManager.addSongs(count: playlist.videoCount!, youtubePlaylist: listId);
+    return true;
   }
 
   /// Download a song with progress indicator notification
--- lib/system/sharing_intent.dart
+++ lib/system/sharing_intent.dart
@@ -1,11 +1,5 @@
-import 'package:asterfox/main.dart';
 import 'package:asterfox/system/home_screen_music_manager.dart';
-import 'package:asterfox/utils/youtube_music_utils.dart';
-import 'package:easy_app/utils/languages.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
-import 'package:uuid/uuid.dart';
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class SharingIntent {
   static void init() {
@@ -17,34 +11,6 @@ class SharingIntent {
   static Future<void> addSong(String? text, bool initial) async {
     // Fluttertoast.showToast(msg: "${initial ? "Initial " : ""}Loading from $text");
     if (text == null) return;
-
-    final isPlaylist = await loadPlaylist(text);
-    if (isPlaylist) return;
-
-    VideoId id;
-    try {
-      id = VideoId(text);
-    } on ArgumentError {
-      Fluttertoast.showToast(msg: Language.getText("invalid_url"));
-      return;
-    }
-    HomeScreenMusicManager.addSong(key: const Uuid().v4(), youtubeId: id.value);
-  }
-
-  static final RegExp playlistRegex = RegExp(r"^https?://(www.)?youtube.com/playlist\?((.+=.+&)*)list=([^&]+)");
-  static Future<bool> loadPlaylist(String text) async {
-    if (!playlistRegex.hasMatch(text)) {
-      return false;
-    }
-    final match = playlistRegex.firstMatch(text)!;
-    final listId = match.group(4)!;
-    final yt = YoutubeExplode();
-    final playlist = await yt.playlists.get(listId);
-    if (playlist.videoCount == null || playlist.videoCount == 0) {
-      Fluttertoast.showToast(msg: Language.getText("external_playlist_empty"));
-      return true;
-    }
-    await HomeScreenMusicManager.addSongs(count: playlist.videoCount!, youtubePlaylist: listId);
-    return true;
+    HomeScreenMusicManager.addSongBySearch(text);
   }
 }
